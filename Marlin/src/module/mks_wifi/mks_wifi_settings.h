f36ddab7e5a484d8573cce64f95a1935bc5bfdd6
Malderin <52313714+Malderin@users.noreply.github.com>
2020-10-06 14:30:08 +0300

    Настройки осей

--- Marlin/src/module/mks_wifi/mks_wifi_settings.h
+++ Marlin/src/module/mks_wifi/mks_wifi_settings.h
@@ -1,7 +1,7 @@
 #ifndef MKS_WIFI_SETTINGS_H
 #define MKS_WIFI_SETTINGS_H
 
-#define MKS_WIFI_ENABLED_WIFI_CONFIG
+//#define MKS_WIFI_ENABLED_WIFI_CONFIG
 
 #define MKS_WIFI_SSID					"beeline"
 #define MKS_WIFI_KEY					"tuzikiiii"
